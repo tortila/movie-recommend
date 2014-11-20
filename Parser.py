db695f1774ef2c570b763cc9b104698f35f906d8
tortila <g.oliwia@gmail.com>
2014-11-20 10:39:02 +0100

    code skeleton

---
+++ Parser.py
@@ -0,0 +1,14 @@
+__author__ = 'oliwia'
+
+class Parser:
+
+    def __init__(self):
+        pass
+
+    # for *.data and *.test files
+    def csv_parse(self, filename):
+        pass
+
+    # for *.moviename files
+    def moviename_parse(self, filename):
+        pass
---
+++ movie-recommend.py
@@ -0,0 +1,11 @@
+__author__ = 'oliwia'
+from Parser import Parser
+
+
+
+def main():
+    print "Welcome to movie-recommend"
+    parser = Parser()
+
+if __name__ == "__main__":
+    main()
