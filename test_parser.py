78827409f50df7a2f9f1d635c370f6e586e1959c
Oliwia Zaremba <oliwiagolec@gmail.com>
2019-06-03 22:50:13 +0200

    Reuse pytest fixtures

--- test_parser.py
+++ test_parser.py
@@ -1,58 +1,17 @@
 import numpy as np
 import pytest
-from hypothesis import given, strategies as st
-from hypothesis_csv.strategies import csv as csv_st
 
 from parser import Parser, Mode
 
 
-@pytest.fixture(autouse=True)
-@given(
-    csv_test=csv_st(
-        columns=[
-            st.integers(min_value=1, max_value=5),
-            st.integers(min_value=1, max_value=5),
-            st.integers(min_value=1, max_value=5),
-        ],
-        lines=20,
-    ),
-    csv_training=csv_st(
-        columns=[
-            st.integers(min_value=1, max_value=20),
-            st.integers(min_value=1, max_value=10),
-            st.integers(min_value=1, max_value=5),
-        ],
-        lines=100,
-    ),
-)
-def fake_data(csv_training, csv_test, mocker, tmp_path):
-    test_data_baseline = tmp_path / f"baseline.test"
-    test_data_improved = tmp_path / f"improved.test"
-    test_data_baseline.write_text(csv_test)
-    test_data_improved.write_text(csv_test)
-    training_data_baseline = tmp_path / f"baseline.training"
-    training_data_improved = tmp_path / f"improved.training"
-    training_data_baseline.write_text(csv_training)
-    training_data_improved.write_text(csv_training)
-    movie_data_baseline = tmp_path / f"baseline.moviename"
-    movie_data_improved = tmp_path / f"improved.moviename"
-    movie_data_baseline.write_text(
-        "\n".join([f"{i};Random Title {i}" for i in range(1, 21)])
-    )
-    movie_data_improved.write_text(
-        "\n".join([f"{i};Random Title {i}" for i in range(1, 21)])
-    )
-    mocker.patch("parser.DIR", tmp_path)
-
-
 @pytest.mark.parametrize("mode", [Mode.BASELINE, Mode.IMPROVED])
-def test_parses_test_data(mode):
+def test_parses_test_data(mode, mock_csv_files):
     p = Parser.from_mode(mode)
     assert len(p.test_set) > 0, "number of test points should be greater than 0"
 
 
 @pytest.mark.parametrize("mode", [Mode.BASELINE, Mode.IMPROVED])
-def test_parses_training_data(mode):
+def test_parses_training_data(mode, mock_csv_files):
     p = Parser.from_mode(mode)
     data = p.training_matrix
     assert np.count_nonzero(data), "number of training points should be greater than 0"
--- test_predictor.py
+++ test_predictor.py
@@ -1,22 +1,11 @@
 import pytest
-from hypothesis import given
-from hypothesis.strategies import lists, permutations
 
 from parser import Parser, Mode
 from predictor import Predictor
 
-movie_titles = "\n".join([f"{i};Random Title {i}" for i in range(1, 6)])
-
-
-@pytest.fixture(autouse=True)
-@given(test_data=lists(permutations(range(1, 4)), min_size=5))
-def mock_parser_reading_files(mocker, test_data):
-    mocker.patch("parser._data_parse", return_value=test_data)
-    mocker.patch("parser._moviename_parse", return_value=movie_titles)
-
 
 @pytest.mark.parametrize("mode", [Mode.BASELINE, Mode.IMPROVED])
-def test_rmse_calculated(mode):
+def test_rmse_calculated(mode, mock_parser_reading_files):
     parser = Parser.from_mode(mode)
     predictor = Predictor(mode, parser.training_matrix, parser.test_set)
     assert predictor.rmse_test > 0.0
