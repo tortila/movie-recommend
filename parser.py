4ee374526a38ea282ed32c99473e0f72a65cae54
Oliwia Zaremba <oliwiagolec@gmail.com>
2019-06-03 23:21:52 +0200

    Fix return types

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 import csv
 from enum import Enum
-from typing import NamedTuple
+from os import PathLike
+from pathlib import Path
+from typing import NamedTuple, List
 
 import numpy as np
 
@@ -8,6 +10,9 @@ DIR = "data"
 DATA = "training"
 MOVIE_NAMES = "moviename"
 TEST = "test"
+
+UmrList = List[List[int]]
+UmrMatrix = np.ndarray
 
 
 class Mode(Enum):
@@ -23,16 +28,24 @@ class Parser(NamedTuple):
         return Parser(file_mode=mode.value)
 
     @property
-    def training_matrix(self):
-        return self._build_umr_matrix(_data_parse(f"{DIR}/{self.file_mode}.{DATA}"))
+    def training_matrix(self) -> UmrMatrix:
+        training_file = Path(f"{DIR}/{self.file_mode}.{DATA}")
+        return self._build_umr_matrix(_data_parse(training_file))
 
     @property
-    def test_set(self):
-        return _data_parse(f"{DIR}/{self.file_mode}.{TEST}")
+    def test_set(self) -> UmrList:
+        test_set_file = Path(f"{DIR}/{self.file_mode}.{TEST}")
+        return _data_parse(test_set_file)
 
-    def _build_umr_matrix(self, raw_data):
+    def _build_umr_matrix(self, raw_data) -> UmrMatrix:
+        """
+        Build UMR Matrix from a list of entries.
+        :param raw_data: a list of lists [u, m, r]
+        :return: a 2-dimensional array A with A[u][m] = r
+        """
         users_number = max(map(lambda x: x[0], raw_data))
-        titles = _moviename_parse(f"{DIR}/{self.file_mode}.{MOVIE_NAMES}")
+        movienames_file = Path(f"{DIR}/{self.file_mode}.{MOVIE_NAMES}")
+        titles = _moviename_parse(movienames_file)
         movies_number = len(titles)
         matrix = np.zeros((users_number, movies_number))
         for row in raw_data:
@@ -40,7 +53,7 @@ class Parser(NamedTuple):
         return matrix
 
 
-def _moviename_parse(filename):
+def _moviename_parse(filename: PathLike) -> List[str]:
     """
     Method for parsing *.moviename files (m, title)
     :param filename: name of the file to parse
@@ -55,7 +68,7 @@ def _moviename_parse(filename):
     return data
 
 
-def _data_parse(filename):
+def _data_parse(filename: PathLike) -> UmrList:
     """
     Method for parsing *.training and *.test files (u, m, r)
     :param filename: name of the file to parse
